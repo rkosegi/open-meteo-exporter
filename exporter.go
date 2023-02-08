7a038743ac2af96be06afd015ee88aad1e9d8376
Richard Kosegi <richard.kosegi@gmail.com>
2023-02-08 18:23:58 +0100

    Use correct gokit imports

--- exporter.go
+++ exporter.go
@@ -19,8 +19,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/go-kit/kit/log/level"
 	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
 	"github.com/prometheus/client_golang/prometheus"
 	"io"
 	"net/http"
